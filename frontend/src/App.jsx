85162498cad77f303411ffee1cfad0dbb1719165
Suraj Thapar <suraj@saavn.com>
2025-07-10 13:04:16 +0530

    Fixed tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,8 @@ import './App.css'
 
 export default function App() {
   return (
-    <div className="h-screen bg-[#2b2d36] flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-pink-500">
+    <div className="h-screen bg-sky-500 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-sky-700">
         Tailwind is working! 🚀
       </h1>
     </div>
