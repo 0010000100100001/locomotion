1a6deeef91867a91860bcb5bc670ac7c0300549c
Suraj Thapar <suraj@saavn.com>
2025-07-10 12:49:54 +0530

    project init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="h-screen bg-[#2b2d36] flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-pink-500">
+        Tailwind is working! 🚀
+      </h1>
+    </div>
+  );
+}
+
