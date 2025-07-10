65818ab5763514bdc363c64bce69bf181589c9be
Suraj Thapar <suraj@saavn.com>
2025-07-10 15:08:31 +0530

    fixed layout in mobile

--- frontend/src/layout/MockAppLayout.jsx
+++ frontend/src/layout/MockAppLayout.jsx
@@ -2,11 +2,14 @@ import BottomNav from "./BottomNav";
 
 export default function MockAppLayout({ children }) {
   return (
-    <div className="w-full h-screen mx-auto bg-[#2b2d36] text-white flex flex-col shadow-xl border border-gray-700 overflow-hidden">
+    <div
+      className="w-full h-screen bg-[#2b2d36] text-white flex flex-col shadow-xl border border-gray-700 overflow-hidden"
+      style={{ height: '100dvh' }}
+    >
       {/* Status Bar */}
       <div className="h-5 bg-[#2b2d36]" />
 
-      {/* Fixed Tabs */}
+      {/* Fixed Tabs (Music, Podcasts, etc.) */}
       <div className="px-4 py-2 bg-[#2b2d36] border-b border-gray-700">
         <div className="flex space-x-6 text-sm font-medium text-gray-300">
           <span className="text-white border-b-2 border-teal-400 pb-1">Music</span>
@@ -20,7 +23,7 @@ export default function MockAppLayout({ children }) {
         {children}
       </div>
 
-      {/* Bottom Nav */}
+      {/* Bottom Navigation */}
       <BottomNav />
     </div>
   );
