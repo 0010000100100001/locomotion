d9ea4217e3420b3090c09501c976edd3cf8d9fba
Suraj Thapar <suraj@saavn.com>
2025-07-11 15:37:31 +0530

    fixed scroll on showcase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,23 +17,25 @@ useEffect(() => {
 
   return (
     <MockAppLayout>
-      {skeletonData?.placement === "showcase" && <Showcase skeletonData={skeletonData} />}
+      <div className="flex-1 overflow-y-auto">
+        {skeletonData?.placement === "showcase" && <Showcase skeletonData={skeletonData} />}
 
-      <div className="flex-1 overflow-y-auto px-4 py-3 space-y-4">
-        {/* Recently Played */}
-        <SectionTitle title="Recently Played" />
-        <div className="flex overflow-x-auto space-x-4">
-          <Card image="https://c.saavncdn.com/940/Shivers-English-2021-20211022044815-500x500.jpg" text="Shivers" />
-          <Card image="https://c.saavncdn.com/artists/Ellie_Goulding_006_20221105190218_500x500.jpg" text="Ellie Goulding" />
-          <Card image="https://c.saavncdn.com/artists/Shakira_002_20220916145812_500x500.jpg" text="Shakira" />
-        </div>
+        <div className="px-4 py-3 space-y-4">
+          {/* Recently Played */}
+          <SectionTitle title="Recently Played" />
+          <div className="flex overflow-x-auto space-x-4">
+            <Card image="https://c.saavncdn.com/940/Shivers-English-2021-20211022044815-500x500.jpg" text="Shivers" />
+            <Card image="https://c.saavncdn.com/artists/Ellie_Goulding_006_20221105190218_500x500.jpg" text="Ellie Goulding" />
+            <Card image="https://c.saavncdn.com/artists/Shakira_002_20220916145812_500x500.jpg" text="Shakira" />
+          </div>
 
-        {/* New Releases */}
-        <SectionTitle title="New Releases" />
-        <div className="flex overflow-x-auto space-x-4">
-          <Card image="https://c.saavncdn.com/760/Sapphire-English-2025-20250623223610-500x500.jpg" text="Sapphire" />
-          <Card image="https://c.saavncdn.com/182/Pehla-Tu-Duja-Tu-From-Son-Of-Sardaar-2-Hindi-2025-20250707201004-500x500.jpg" text="Pehla Tu Duja Tu" />
-          <Card image="https://c.saavncdn.com/279/F1-The-Album-Deluxe-English-2025-20250624223443-500x500.jpg" text="F1 The Album" />
+          {/* New Releases */}
+          <SectionTitle title="New Releases" />
+          <div className="flex overflow-x-auto space-x-4">
+            <Card image="https://c.saavncdn.com/760/Sapphire-English-2025-20250623223610-500x500.jpg" text="Sapphire" />
+            <Card image="https://c.saavncdn.com/182/Pehla-Tu-Duja-Tu-From-Son-Of-Sardaar-2-Hindi-2025-20250707201004-500x500.jpg" text="Pehla Tu Duja Tu" />
+            <Card image="https://c.saavncdn.com/279/F1-The-Album-Deluxe-English-2025-20250624223443-500x500.jpg" text="F1 The Album" />
+          </div>
         </div>
       </div>
     </MockAppLayout>
